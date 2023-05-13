b080ad3f7fd089f7f7b562b0610943b5f23fca4c
thCorOli <93624142+thCorOli@users.noreply.github.com>
2023-05-13 19:34:12 -0300

    Add files via upload

---
+++ primosNaive/mpi_Send_Recv.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+#include <math.h>
+
+int primo(long int n) {
+    int i;
+       
+    for (i = 3; i < (int)(sqrt(n) + 1); i += 2) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    double t_inicial, t_final;
+    int cont = 0, total = 0;
+    long int i, n;
+    int meu_ranque, num_procs, inicio, salto,etiq = 0, flag = 0;
+
+    
+    if (argc < 2) {
+        printf("Valor inválido! Entre com um valor do maior inteiro\n");
+        return 0;
+    } else {
+        n = strtol(argv[1], (char **) NULL, 10);
+    }
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    t_inicial = MPI_Wtime();
+    inicio = 3 + meu_ranque * 2;
+    salto = num_procs * 2;
+    for (i = inicio; i <= n; i += salto) {
+        if (primo(i) == 1) cont++;
+    }
+        
+    if (num_procs > 1) {
+        if (meu_ranque != 0) {
+            MPI_Send(&cont, 1, MPI_INT, etiq, flag, MPI_COMM_WORLD);
+        } else {
+            total = cont;
+            for (int source = 1; source < num_procs; source++) {
+                MPI_Recv(&cont, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                total += cont;
+            }
+        }
+    } else {
+        total = cont;
+    }
+    
+    t_final = MPI_Wtime();
+
+    if (meu_ranque == 0) {
+        total += 1;    /* Acrescenta o dois, que também é primo */
+        printf("Quant. de primos entre 1 e n: %d \n", total);
+        printf("Tempo de execucao: %1.7f \n", t_final - t_inicial);     
+    }
+
+    MPI_Finalize();
+    return 0;
+}
